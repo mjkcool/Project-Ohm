b04014140de9fee20065a80d5bba273da5ac059a
leeseoyeon <s2019w14@e-mirim.hs.kr>
2021-03-24 00:43:46 +0900

    로그인페이지, 회원가입 페이지 완성 (넘기는거는 아직불가 버튼이름 찾을 수 없는 에러발생)

---
+++ app/src/main/java/kr/hs/emirim/ohm/SubActivity.kt
@@ -0,0 +1,13 @@
+package kr.hs.emirim.ohm
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SubActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
