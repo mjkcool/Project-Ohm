a7ab1673b20965c0bfa603fc8ba16df21d15f783
leeseoyeon <s2019w14@e-mirim.hs.kr>
2021-04-03 02:48:00 +0900

    로그인 화면연결 구현완료 (혹시 모르니 실행한번씩 부탁드립니다)

--- app/src/main/java/kr/hs/emirim/ohm/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/ohm/MainActivity.kt
@@ -3,20 +3,23 @@ package kr.hs.emirim.ohm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        login_button.setOnClickListener {
-            val intent = Intent(this, login::class.java)
-            startActivity(intent)
+        button = findViewById(R.id.login_button) as Button
         }
 
-        join_button.setOnClickListener {
-            val intent = Intent(this, join::class.java)
-            startActivity(intent)
-        }
+    fun onClickButton(view : View){
+        var intent = Intent(this, login::class.java)
+        startActivity(intent)
     }
+
 }
