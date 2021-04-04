031d57bc28e90b5ee5ebf3bde1a5fc7ec0baa4c9
sxxrxm <s2019w19@e-mirim.hs.kr>
2021-04-04 13:38:22 +0900

    이메일 회원가입, 이메일 로그인 메소드 구현 완료.

--- app/src/main/java/kr/hs/emirim/ohm/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/ohm/MainActivity.kt
@@ -3,11 +3,20 @@ package kr.hs.emirim.ohm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val TAG : String = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val login_button = findViewById<Button>(R.id.login_button)
+        val googlelogin_button = findViewById<Button>(R.id.googlelogin_button)
+        val join_button = findViewById<Button>(R.id.join_button)
 
         login_button.setOnClickListener {
             val intent = Intent(this, login::class.java)
@@ -18,5 +27,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, join::class.java)
             startActivity(intent)
         }
+
+        googlelogin_button.setOnClickListener{
+            //이건 나중에 구현할게용...
+        }
+
     }
 }
