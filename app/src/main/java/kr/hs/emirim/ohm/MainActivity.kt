30086dcae29026a10bc28f36a5825633eb4f74b5
lee-seo-yeon <s2019w14@e-mirim.hs.kr>
2021-04-05 17:49:37 +0900

    회원가입 안되는 에러 고치는중.....

--- app/src/main/java/kr/hs/emirim/ohm/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/ohm/MainActivity.kt
@@ -3,30 +3,21 @@ package kr.hs.emirim.ohm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
 
-        login_button.setOnClickListener({
-            val intent = Intent(this, login::class.java)
-            startActivity(intent)
-        })
+        login_button.setOnClickListener {
+            startActivity(Intent(this, login::class.java))
+            finish()
+        }
 
-        join_button.setOnClickListener({
-            val intent = Intent(this, join::class.java)
-            startActivity(intent)
-        })
+        join_button.setOnClickListener {
+            startActivity(Intent(this, join::class.java))
+            finish()
+        }
     }
-
-//    fun onClickButton(view : View){
-//        var intent = Intent(this, login::class.java)
-//        startActivity(intent)
-//    }
-
 }
