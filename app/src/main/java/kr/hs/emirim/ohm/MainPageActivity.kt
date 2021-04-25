3615b060caddbfd1bac87415f1897cc3212f2b30
ryung <johyunryung@naver.com>
2021-04-26 07:50:04 +0900

    src/main MainPageActivity.kt 생성

--- app/src/main/java/kr/hs/emirim/ohm/MainPageActivity.kt
+++ app/src/main/java/kr/hs/emirim/ohm/MainPageActivity.kt
@@ -3,9 +3,9 @@ package kr.hs.emirim.ohm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_mainpage)
     }
 }
