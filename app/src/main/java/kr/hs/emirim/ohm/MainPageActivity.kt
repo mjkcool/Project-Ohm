413292040c8a164539e06e77f2205af0ed5fa6f8
ryung <johyunryung@naver.com>
2021-04-27 00:34:47 +0900

    res/drawable main_fontground.xml, main_profile.xml 생성 src/main MainPageActivity.kt 생성

--- app/src/main/java/kr/hs/emirim/ohm/MainPageActivity.kt
+++ app/src/main/java/kr/hs/emirim/ohm/MainPageActivity.kt
@@ -3,7 +3,7 @@ package kr.hs.emirim.ohm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
