ed068e5252cb23380f03e5dafd0f010b3e66b28e
s2019w05 <s2019w05@e-mirim.hs.kr>
2021-04-30 16:57:44 +0900

    대기실 화면 구현, Recycler View(참여자 명단 보기) 제외

--- app/src/main/java/kr/hs/emirim/ohm/MainPage.kt
+++ app/src/main/java/kr/hs/emirim/ohm/MainPage.kt
@@ -7,22 +7,22 @@ import java.util.*
 
 class MainPage : AppCompatActivity() {
     var viewPager: ViewPager? = null
-    var adapter: Adapter? = null
+//    var adapter: Adapter? = null
     var models: MutableList<Model>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        adapter = Adapter(models!!, this)
-        viewPager = findViewById(R.id.viewPager)
-        viewPager.setAdapter(adapter)
-        viewPager.setClipToPadding(false)
-        viewPager.setPadding(0, 0, 50, 0)
-        viewPager.setPageMargin(30)
-
-        models = ArrayList()
-        (models as ArrayList<Model>).add(Model(R.drawable.inmain_people, "목적지로 가는 길", "2021.04.22", "5일전"))
-        (models as ArrayList<Model>).add(Model(R.drawable.inmain_people, "반민초 vs 민초", "2021.04.26", "하루 전"))
-        (models as ArrayList<Model>).add(Model(R.drawable.inmain_people, "엄마 vs 아빠", "2021.04.01", "26일전"))
+        setContentView(R.layout.activity_waiting_room)
+//        adapter = Adapter(models!!, this)
+//        viewPager = findViewById(R.id.viewPager)
+//        viewPager.setAdapter(adapter)
+//        viewPager.setClipToPadding(false)
+//        viewPager.setPadding(0, 0, 50, 0)
+//        viewPager.setPageMargin(30)
+//
+//        models = ArrayList()
+//        (models as ArrayList<Model>).add(Model(R.drawable.inmain_people, "목적지로 가는 길", "2021.04.22", "5일전"))
+//        (models as ArrayList<Model>).add(Model(R.drawable.inmain_people, "반민초 vs 민초", "2021.04.26", "하루 전"))
+//        (models as ArrayList<Model>).add(Model(R.drawable.inmain_people, "엄마 vs 아빠", "2021.04.01", "26일전"))
 
 
     }
