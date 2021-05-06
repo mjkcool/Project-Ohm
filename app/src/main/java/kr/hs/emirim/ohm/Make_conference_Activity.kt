7f6e243487bda1cae2f0bf1c325b498346d47a72
ryung <johyunryung@naver.com>
2021-05-07 04:36:20 +0900

    main/java 회의생성 할 때 보여주는 Make_conference_Activity.kt 생성 및 주석

---
+++ app/src/main/java/kr/hs/emirim/ohm/Make_conference_Activity.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.ohm
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Make_conference_Activity : AppCompatActivity() { //회의 생성 페이지
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) //레이아웃 구현을 하지 못 하여 임시로 메인레이아웃을 둠
+    }
+}
