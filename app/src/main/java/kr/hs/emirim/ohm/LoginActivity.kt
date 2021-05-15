76c776ff35eea46e219065775883f2132ae7b4fe
s2019w05 <s2019w05@e-mirim.hs.kr>
2021-05-15 20:56:53 +0900

    (LoginActivity.kt) ready to connect Google Firebase

--- app/src/main/java/kr/hs/emirim/ohm/LoginActivity.kt
+++ app/src/main/java/kr/hs/emirim/ohm/LoginActivity.kt
@@ -2,10 +2,24 @@ package kr.hs.emirim.ohm
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class LoginActivity:AppCompatActivity() {
+class LoginActivity: AppCompatActivity() {
+
+    lateinit var googleLoginBtn: FloatingActionButton
+    lateinit var facebookLoginBtn: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        googleLoginBtn.setOnClickListener() {
+
+        }
+
+        facebookLoginBtn.setOnClickListener() {
+
+        }
+
     }
 }
