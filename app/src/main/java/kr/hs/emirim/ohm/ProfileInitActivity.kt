01c021fbe028ec213223ac80a2caaef215d18f89
s2019w05 <s2019w05@e-mirim.hs.kr>
2021-05-15 14:19:36 +0900

    activity_login.xml 뷰 80% 완성, 사용자 정보 초기설정 닉네임 뷰와 한줄소개 뷰 Fragment 완성

--- app/src/main/java/kr/hs/emirim/ohm/ProfileInitActivity.kt
+++ app/src/main/java/kr/hs/emirim/ohm/ProfileInitActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kr.hs.emirim.ohm.ui.main.ProfileInitFragment
 
-class ProfileInit : AppCompatActivity() {
+class ProfileInitActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
