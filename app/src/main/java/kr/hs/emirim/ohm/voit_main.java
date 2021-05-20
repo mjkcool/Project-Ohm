0b98e2bba5e14e351f81cd027dabb073649730f3
ryung <johyunryung@naver.com>
2021-05-21 02:22:01 +0900

    src/main voit_main.java 투표를 할 수 있는 창 생성

---
+++ app/src/main/java/kr/hs/emirim/ohm/voit_main.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.ohm;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class voit_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.voit_main);
+
+    }
+}
