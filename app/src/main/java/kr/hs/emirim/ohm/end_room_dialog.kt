516643a7a03905f5d8ee3de3ca9a98fcd7810daf
lee-seo-yeon <s2019w14@e-mirim.hs.kr>
2021-05-31 16:25:21 +0900

    모달 기능작업 하는중

--- app/src/main/java/kr/hs/emirim/ohm/end_room_dialog.kt
+++ app/src/main/java/kr/hs/emirim/ohm/end_room_dialog.kt
@@ -1,11 +1,18 @@
 package kr.hs.emirim.ohm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 
-class end_room_dialog : AppCompatActivity() {
+class end_room_dialog(context:Context): Dialog(context) {
+    val TAG: String = "로그"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_end_room_dialog)
+        window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
     }
 }
