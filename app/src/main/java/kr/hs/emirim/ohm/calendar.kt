f0f374d0670636be6ccfe6332d8842cbf5a8c64b
jiyeon1117 <s2019w24@e-mirim.hs.kr>
2021-06-03 05:15:15 +0900

    캘린더 위 아래로 슬라이드 화면 구현 + create_calendar 파일 삭제

--- app/src/main/java/kr/hs/emirim/ohm/calendar.kt
+++ app/src/main/java/kr/hs/emirim/ohm/calendar.kt
@@ -3,27 +3,39 @@ package kr.hs.emirim.ohm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
+import android.view.animation.TranslateAnimation
 import android.widget.CalendarView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_calendar.*
 
 class calendar : AppCompatActivity() {
 
-    lateinit var calendarView: CalendarView
-    lateinit var date: TextView
+    private lateinit var create_intent: ConstraintLayout
+    private lateinit var create_window: CardView
+    private lateinit var calendarView: CalendarView
+    private lateinit var date: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
 
-        calendarView = findViewById(R.id.calendarView)
+        date = findViewById(R.id.date)  //날짜
+        calendarView = findViewById(R.id.calendarView)  //캘린더
+        create_intent = findViewById(R.id.create_intent)    //일정 생성
+        create_window = findViewById(R.id.create_window)    //일정 생성 창
 
-        date = findViewById(R.id.date)
+        create_intent.setOnClickListener (View.OnClickListener {
+            create_window.visibility = View.VISIBLE
+            val animation = AnimationUtils.loadAnimation(this, R.anim.translate_bottom)
+            create_window.startAnimation(animation)
+        })
 
+        //캘린더 클릭 시 날짜 확인
         calendarView.setOnDateChangeListener { view, year, month, dayOfMonth -> date
-            date.visibility = View.VISIBLE
-
-            date.text = String.format("%d년 %d월 %d일", year, month + 1, dayOfMonth)
+             date.text = String.format("%d월 %d일", month + 1, dayOfMonth)
         }
     }
 }
